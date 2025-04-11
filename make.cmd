7bdbeb71e883e5eeb97f815fb31a5df8d5640e27
Maksymilian Wilczyński <makswilczynski2010@gmail.com>
2025-04-11 20:51:12 +0200

    color, logo, realistic handling

--- make.cmd
+++ make.cmd
@@ -1,95 +1,99 @@
 @echo off
-title compiling
+title batch build system
 setlocal enabledelayedexpansion
-if "%~1"=="" (set "execname=main.exe") else set "execname=%~1"
-set filelist=filelist.txt
-set config=config.txt
 
-if not exist %filelist% (
-    goto listmissing
-) else if "%filelist%" == "0" ( 
-    goto listempty
+:: Set ESC variable
+call :setESC
+
+:: Predefine common colors using delayed expansion-safe syntax
+set "RESET=!ESC![0m"
+set "RED=!ESC![31m"
+set "GREEN=!ESC![32m"
+set "YELLOW=!ESC![33m"
+set "CYAN=!ESC![36m"
+
+:: Fancy header
+set "BANNER_COLOR=!ESC![1;94m"  :: bold blue
+set "SUB_COLOR=!ESC![2;37m"     :: dim white
+set "RESET=!ESC![0m"
+
+cls
+echo !BANNER_COLOR!
+echo            _           _       _        _           _ _     _                  _
+echo           ^| ^|         ^| ^|     ^| ^|      ^| ^|         (_) ^|   ^| ^|                ^| ^|
+echo    __ _   ^| ^|__   __ _^| ^|_ ___^| ^|__    ^| ^|__  _   _ _^| ^| __^| ^|   ___ _   _ ___^| ^|_ ___ _ __ ___
+echo   / _` ^|  ^| '_ \ / _` ^| __/ __^| '_ \   ^| '_ \^| ^| ^| ^| ^| ^|/ _` ^|  / __^| ^| ^| / __^| __/ _ \ '_ ` _ \
+echo  ^| (_^| ^|  ^| ^|_) ^| (_^| ^| ^|^| (__^| ^| ^| ^|  ^| ^|_) ^| ^|_^| ^| ^| ^| (_^| ^|  \__ \ ^|_^| \__ \ ^|^|  __/ ^| ^| ^| ^| ^|
+echo   \__,_^|  ^|_.__/ \__,_^|\__\___^|_^| ^|_^|  ^|_.__/ \__,_^|_^|_^|\__,_^|  ^|___/\__, ^|___/\__\___^|_^| ^|_^| ^|_^|
+echo                                                                       __/ ^|
+echo                                                                      ^|___/
+echo !RESET!
+echo.
+
+:: Default executable name
+set "execname=main.exe"
+if not "%~1"=="" set "execname=%~1"
+
+set "filelist=filelist.txt"
+set "config=config.txt"
+
+:: Check if file list exists
+if not exist "%filelist%" (
+    echo !RED!Error: File list "%filelist%" is missing!%RESET%
+    goto waitexit
 )
 
-set /a linecount=0
-set /a currentline=1
-for /f "tokens=*" %%a in (%filelist%) do (
-    set line[!currentline!]=%%a
-    set /a linecount+=1
-    set /a currentline+=1
+:: Read config flags (first line only)
+set "flags="
+for /f "usebackq tokens=*" %%c in ("%config%") do (
+    set "flags=%%c"
+    goto :breakcfg
+)
+:breakcfg
+
+:: Compile source files
+set "obj_files="
+for /f "usebackq tokens=*" %%f in ("%filelist%") do (
+    set "src=%%f.cpp"
+    set "obj=%%f.o"
+    echo !CYAN!Compiling !src!...!RESET!
+    g++ -c "!src!" -o "!obj!" !flags!
+    if errorlevel 1 (
+        echo !RED!Error: Failed to compile !src!!RESET!
+        goto waitexit
+    )
+    set "obj_files=!obj_files! !obj!"
 )
 
-set /a linecount2=0
-set /a currentline2=1
-for /f "tokens=*" %%a in (%config%) do (
-    set configline[!currentline2!]=%%a
-    set /a linecount2+=1
-    set /a currentline2+=1
+:: Linking
+echo !CYAN!Linking into "%execname%"...!RESET!
+g++ -o "%execname%" !obj_files!
+if errorlevel 1 (
+    echo !RED!Error: Linking failed!%RESET%
+    goto waitexit
 )
 
-set "link_files="
-:: variable to store all files to link
-for /l %%i in (1,1,%linecount%) do (
-    set "link_files=!link_files! !line[%%i]!.o"
-)
-
-:compile
-:: compile part of the program
-for /l %%i in (1,1,%linecount%) do (
-    set "source_file=!line[%%i]!.cpp"
-    set "output_file=!line[%%i]!.o"
-    echo compiling !source_file!
-    g++ -c "!source_file!" -o "!output_file!" %configline[0]% || goto compileerror
-)
-
-goto link
-
-:link
-:: link part of the program
-echo linking...
-g++ -o "%execname%" %link_files% || goto linkerror
-:: linked all files
-goto strip
-
-:strip
-echo stripping...
+:: Stripping
+echo !CYAN!Stripping binary...!RESET!
 strip "%execname%"
-goto clean
 
-:clean
-echo cleaning...
-del /Q %link_files%
-goto exit
+:: Cleaning
+echo !CYAN!Cleaning up...!RESET!
+del /Q !obj_files!
 
-:compileerror
-:: a compile error has occurred, exit
-echo a compiling error has occurred!
-echo press any key to exit..
-pause > nul
-exit 1
+:: The exit
+echo !GREEN!Done!RESET!
+timeout /t 2 /nobreak > NUL
+goto :eof
 
-:linkerror
-:: a link error has occurred, exit
-echo a link error has occurred!
-echo press any key to exit..
-pause > nul
-exit 1
+:waitexit
+echo !YELLOW!Press any key to exit...!RESET!
+pause >nul
+exit /b
 
-:listmissing
-:: file list is missing, exit
-echo file list is missing!
-echo press any key to exit..
-pause > nul
-exit 1
-
-:listempty
-:: file list is empty, exit
-echo file list is empty!
-echo press any key to exit..
-pause > nul
-exit 1
-
-:exit
-echo press any key to exit..
-pause > nul
-:: exit
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)
+exit /B 0
